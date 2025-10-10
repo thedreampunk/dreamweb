bfe07ceb4ae77ee73fabee0a363b86cd59b73435
Dreampunk <axel.bourdet@gmail.com>
2025-10-10 17:56:38 +0200

    WIP on index_test EN version : made the display prettier, fixed some display issues. Getting there !

---
+++ js/dreamjs.js
@@ -0,0 +1,18 @@
+function isSmartphone() {
+    // User agent check for mobile, but not tablet
+    var ua = navigator.userAgent;
+    var isMobileUA = /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(ua);
+    var isTabletUA = /iPad|Tablet|PlayBook|Silk/i.test(ua);
+    // Screen width check (phones are usually <= 767px)
+    var isPhoneScreen = window.innerWidth <= 767;
+    return isMobileUA && !isTabletUA && isPhoneScreen;
+}
+window.addEventListener('DOMContentLoaded', function () {
+    if (isSmartphone()) {
+        var video = document.getElementById('myVideo');
+        var source = document.getElementById('videoSource');
+        source.src = '../vid/cotton_mobile.mp4'; // <-- your vertical video path
+        video.classList.add('mobile-vertical-video');
+        video.load();
+    }
+});
