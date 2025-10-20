5228656908c988610d8a9746efccb5940d9534d0
Dreampunk <axel.bourdet@gmail.com>
2025-10-20 15:45:37 +0200

    Update : Dreamradio buttons better behaviour + Always active footer with social links. EN page only

--- js/dreamjs.js
+++ js/dreamjs.js
@@ -1,28 +1,29 @@
-// Custom display for mobile devices
+// Screen width phones are usually <= 767px
+const USUAL_PHONE_WIDTH = 767
+
+// Function checking if the device is a smartphone
 function isSmartphone() {
     // User agent check for mobile, but not tablet
     var ua = navigator.userAgent;
     var isMobileUA = /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(ua);
     var isTabletUA = /iPad|Tablet|PlayBook|Silk/i.test(ua);
-    // Screen width check (phones are usually <= 767px)
-    var isPhoneScreen = window.innerWidth <= 767;
+    // Screen width check
+    var isPhoneScreen = window.innerWidth <= USUAL_PHONE_WIDTH;
     return isMobileUA && !isTabletUA && isPhoneScreen;
 }
+
+// What to do if the device is a smartphone
 window.addEventListener('DOMContentLoaded', function () {
     if (isSmartphone()) {
-        // Background video portrait
+        // Background video should be portrait mode
         var video = document.getElementById('myVideo');
         var source = document.getElementById('videoSource');
         source.src = '../vid/cotton_mobile.mp4';
         source.style = 'filter:opacity(70%)'
         video.classList.add('mobile-vertical-video');
         video.load();
-        
-        // Activer un joli footer :)
+        // Activate the sweet mobile footer
 
-        // Masquer section pokémon go aka MISC section
-        //var otherSection = document.getElementById('other')
-        //otherSection.style.display = "none";
     }
 });
 
@@ -37,6 +38,10 @@ window.onSpotifyIframeApiReady = (IFrameAPI) => {
         playlist => {
         playlist.addEventListener('click', () => {
             EmbedController.loadUri(playlist.dataset.spotifyId)
+            // untoggle previous active button
+            document.querySelectorAll('.playlist').forEach(function(b){ b.classList.remove('active'); });
+            // toggle active button
+            playlist.classList.add('active');
             });
         })
     };
